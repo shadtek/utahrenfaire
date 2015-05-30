f6db9ca7988bc44b3d6feb6f853ddbc23c8d26af
Spencer Shattuck <spencer.shattuck@gmail.com>
2015-05-30 13:44:26 -0400

    Fixes server.js

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ var express = require('express');
 var port = process.env.EXPRESS_PORT || 9001;
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.all('*', function(req, res, next) {
     res.sendFile('/public/index.html', { root: __dirname });
 });
 
-app.listen(port);
+app.listen(port, function(err){
+  console.log('connected at ', port);
+});
