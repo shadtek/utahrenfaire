1c215309a9f5f1360b2bb307495cf425a754f19d
shadtek <spencer.shattuck@gmail.com>
2015-06-07 11:47:09 -0600

    entertainers view is set up and look good

--- angular/mainService.js
+++ angular/mainService.js
@@ -5,11 +5,11 @@ app.service('mainService', function($firebaseArray, $firebaseObject) {
 	var url = 'https://utahrenfaire.firebaseio.com/';
 	var ref = new Firebase(url);
 	var vendors = new Firebase(url + 'vendors');
-	// var mainInfo = new Firebase(url + 'mainInfo');
+	var entertainers = new Firebase(url + 'entertainers')
 
 	this.root = $firebaseArray(ref);
 	this.vendors = $firebaseArray(vendors);
-	// this.mainInfo = $firebaseObject(mainInfo);
+	this.entertainers = $firebaseArray(entertainers);
 
 	this.getMainInfo = function() {
   	return $firebaseObject(new Firebase(url + 'mainInfo'));
--- states/entertainers/entertainersCtrl.js
+++ states/entertainers/entertainersCtrl.js
@@ -2,9 +2,10 @@ var app = angular.module('utahrenfaire');
 
 app.controller('entertainersCtrl', function($scope, mainService) {
 	
-	mainService.$loaded().then(function(data) {
-		console.log(data[0])
-		$scope.entertainers = data[0]
+	$scope.entertainers = mainService.entertainers;
+
+	$scope.entertainers.$loaded().then(function(data) {
+		console.log($scope.entertainers);
 	})
 
 })
