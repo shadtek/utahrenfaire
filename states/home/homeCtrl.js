5b99bab480790d570273e4ad3c4961273a3b0f9a
shadtek <spencer.shattuck@gmail.com>
2015-06-10 19:29:23 -0600

    adding hide/show to the ng-repeat on vendors and entertainers

--- states/home/homeCtrl.js
+++ states/home/homeCtrl.js
@@ -3,11 +3,4 @@ var app = angular.module('utahrenfaire');
 app.controller('homeCtrl', function($scope, mainService, mainInfoRef) {
 	$scope.mainInfo = mainInfoRef;
 
-	// $scope.fbMaininfo = mainService.mainInfo;
-	// $scope.fbMaininfo.$loaded().then(function(data) {
-	// 	$scope.eventPlace = data.eventPlace;
-	// 	$scope.eventDays = data.eventDays;
-	// 	$scope.eventDate = data.eventDate;
-	// })
-
 })
