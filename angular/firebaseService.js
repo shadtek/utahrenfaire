1837e61aa79e5af241f01ea97f3998dc8e0207a7
Spencer Shattuck <spencer.shattuck@gmail.com>
2015-07-10 10:51:24 -0600

    commit from my PC

---
+++ angular/firebaseService.js
@@ -0,0 +1,31 @@
+var app = angular.module('utahrenfaire');
+
+app.service('firebaseService', function ($firebase, $firebaseObject, $firebaseArray) {
+  var firebaseUrl = 'https://utahrenfaire.firebaseio.com/';
+
+  this.getUser = function(userId){
+    return $firebaseObject(new Firebase(firebaseUrl + 'users/' + userId));
+  };
+
+  this.getMainInfo = function() {
+  	return $firebaseObject(new Firebase(firebaseUrl + 'mainInfo'));
+  };
+
+  this.getVendors = function() {
+    return $firebaseArray(new Firebase(firebaseUrl + 'vendors'));
+  };
+
+  this.getEntertainers = function() {
+    return $firebaseArray(new Firebase(firebaseUrl + 'entertainers'));
+  };
+
+  this.getFeastInfo = function() {
+    return $firebaseObject(new Firebase(firebaseUrl + 'feastInfo'));
+  };
+
+  this.getGallery = function() {
+    return $firebaseArray(new Firebase(firebaseUrl + 'gallery'));
+  };
+
+
+})
