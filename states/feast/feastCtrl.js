e093240601a871902d58182d6a10b84df72f6deb
Spencer Shattuck <spencer.shattuck@gmail.com>
2015-09-15 22:52:18 -0600

    Removed old feast page

--- states/feast/feastCtrl.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('utahrenfaire');
-
-app.controller('feastCtrl', function($scope, firebaseService, feastInfoRef) {
-	window.scrollTo(0, 0)
-
-	$scope.feastInfo = feastInfoRef;
-
-})
