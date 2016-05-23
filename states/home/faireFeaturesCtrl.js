af9e9c0fcdba488df5dea9d14cf4f4ce37b86664
shadtek <spencer.shattuck@gmail.com>
2016-05-23 02:53:14 -0600

    work on controllers

--- states/home/faireFeaturesCtrl.js
+++ states/home/faireFeaturesCtrl.js
@@ -1,9 +1,36 @@
 var app = angular.module('utahrenfaire');
 
-app.controller('faireFeaturesCtrl', function($scope, firebaseService, entertainersRef) {
-	
-	$scope.entertainers = entertainersRef;
+app.controller('faireFeaturesCtrl', function($scope, entertainersRef, firebaseService){
+  window.scrollTo(0, 0)
 
-	$scope.$parent.userIsAdmin = "child sees admin";
+  $scope.entertainers = entertainersRef;
 
-})
+  $scope.$parent.userIsAdmin = "child sees admin";
+
+  $scope.addEntertainer = function(item){
+    $scope.entertainers.$add(item);
+    console.log("Added")
+    $scope.item = ""
+  }
+
+  $scope.updateEntertainer = function(item){
+  	if (item.position2){
+      item.position = item.position2;
+      delete item.position2;
+    }
+    $scope.entertainers.$save(item);
+    console.log("Updated")
+  };
+
+  $scope.toggleEntertainerHide = function(item){
+    item.hide = !item.hide;
+    $scope.entertainers.$save(item);
+    console.log("Hide Toggled")
+  };
+
+  $scope.removeEntertainer = function(item){
+    $scope.entertainers.$remove(item);
+    console.log("Deleted")
+  }
+
+});
