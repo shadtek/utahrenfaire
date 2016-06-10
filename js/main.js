24202efeef0207d43c3d14ff8a10845daf70c3ef
shadtek <spencer.shattuck@gmail.com>
2016-06-09 19:27:31 -0600

    remove old code and get ready to remove more

--- js/main.js
+++ js/main.js
@@ -17,45 +17,45 @@
 
 
 	// Carousel Feature Slide
-	var owlCrouselFeatureSlide = function() {
+	// var owlCrouselFeatureSlide = function() {
 		
-		var owl = $('.owl-carousel');
+	// 	var owl = $('.owl-carousel');
 
-		owl.on('initialized.owl.carousel change.owl.carousel',function(elem){
-			var current = elem.item.index;
-			$(elem.target).find(".owl-item").eq(current).find(".to-animate").removeClass('fadeInUp animated');
-			$(elem.target).find(".owl-item").eq(current).find(".to-animate-2").removeClass('fadeInUp animated');
+	// 	owl.on('initialized.owl.carousel change.owl.carousel',function(elem){
+	// 		var current = elem.item.index;
+	// 		$(elem.target).find(".owl-item").eq(current).find(".to-animate").removeClass('fadeInUp animated');
+	// 		$(elem.target).find(".owl-item").eq(current).find(".to-animate-2").removeClass('fadeInUp animated');
 		
-		});
-		owl.on('initialized.owl.carousel changed.owl.carousel',function(elem){
-			setTimeout(function(){
-				var current = elem.item.index;
-				$(elem.target).find(".owl-item").eq(current).find(".to-animate").addClass('fadeInUp animated');
-			}, 700);
-			setTimeout(function(){
-				var current = elem.item.index;
-				$(elem.target).find(".owl-item").eq(current).find(".to-animate-2").addClass('fadeInUp animated');
-			}, 900);
-     	});
-		owl.owlCarousel({
-			items: 1,
-		    loop: true,
-		    margin: 0,
-		    responsiveClass: true,
-		    nav: true,
-		    dots: true,
-		    autoHeight: true,
-		    smartSpeed: 500,
-		    autoplay: true,
-			autoplayTimeout: 5000,
-			autoplayHoverPause: true,
-		    navText: [	
-		      "<i class='icon-arrow-left2 owl-direction'></i>",
-		      "<i class='icon-arrow-right2 owl-direction'></i>"
-	     	]
-		});
+	// 	});
+	// 	owl.on('initialized.owl.carousel changed.owl.carousel',function(elem){
+	// 		setTimeout(function(){
+	// 			var current = elem.item.index;
+	// 			$(elem.target).find(".owl-item").eq(current).find(".to-animate").addClass('fadeInUp animated');
+	// 		}, 700);
+	// 		setTimeout(function(){
+	// 			var current = elem.item.index;
+	// 			$(elem.target).find(".owl-item").eq(current).find(".to-animate-2").addClass('fadeInUp animated');
+	// 		}, 900);
+ //     	});
+	// 	owl.owlCarousel({
+	// 		items: 1,
+	// 	    loop: true,
+	// 	    margin: 0,
+	// 	    responsiveClass: true,
+	// 	    nav: true,
+	// 	    dots: true,
+	// 	    autoHeight: true,
+	// 	    smartSpeed: 500,
+	// 	    autoplay: true,
+	// 		autoplayTimeout: 5000,
+	// 		autoplayHoverPause: true,
+	// 	    navText: [	
+	// 	      "<i class='icon-arrow-left2 owl-direction'></i>",
+	// 	      "<i class='icon-arrow-right2 owl-direction'></i>"
+	//      	]
+	// 	});
 
-	};
+	// };
 
 
 
@@ -445,7 +445,7 @@
 	$(function(){
 
 		burgerMenu();
-		owlCrouselFeatureSlide();
+		// owlCrouselFeatureSlide();
 		clickMenu();
 		windowScroll();
 		navigationSection();
