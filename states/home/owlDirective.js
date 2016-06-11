9196685d1485409e0a1c51adad29124962d779a4
shadtek <spencer.shattuck@gmail.com>
2016-06-11 11:10:16 -0600

    need to be able to get console defined, it's not working yet

--- states/home/owlDirective.js
+++ states/home/owlDirective.js
@@ -4,12 +4,9 @@ app.controller('carouselCtrl', function($scope, carouselRef, firebaseService){
 
   $scope.carousel = carouselRef;
   $scope.item = {};
-  console.log('i am the carouselCtrl');
 
-  // $scope.slideImg = {
-  // 	'background-image':'url($scope.carousel.)', 
-  // 	'background-position': 'center'
-  // };
+  $scope.slideImg = 'images/robbie.jpg'
+
 
   $scope.addCarousel = function(item){
     $scope.carousel.$add(item);
